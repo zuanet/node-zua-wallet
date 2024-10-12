69c6821e0b1320d4fe5bb60f9a058585668c11e0
zuanet <168155305+zuanet@users.noreply.github.com>
2024-10-13 00:54:36 +0300

    Delete wallet directory

--- wallet/initZuaFramework.ts
+++
@@ -1,8 +0,0 @@
-import { Wallet } from './wallet';
-
-export const initZuaFramework = async () => {
-  // console.log("Zua - framework: init");
-  await Wallet.initRuntime();
-  // console.log("Zua - framework: ready");
-};
-
