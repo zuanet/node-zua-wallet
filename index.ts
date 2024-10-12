6c447a32799c9aed9b0e23d5b05ad92a124bad4a
zuanet <168155305+zuanet@users.noreply.github.com>
2024-10-13 00:55:16 +0300

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-import {log, FlowLogger} from "./utils/logger";
-import {Wallet, Storage, zuacore, CONFIRMATION_COUNT, COINBASE_CFM_COUNT} from "./wallet/wallet";
-import {initZuaFramework} from './wallet/initZuaFramework';
-import {EventTargetImpl} from './wallet/event-target-impl';
-import * as helper from './utils/helper';
-
-export {CONFIRMATION_COUNT, COINBASE_CFM_COUNT};
-export {Wallet, initZuaFramework, log, EventTargetImpl, helper, Storage, FlowLogger, zuacore}
